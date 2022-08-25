90a52240282c69feeaade2a5d61340a564ae501a
Bice93 <bianca.cerulo@hotmail.it>
2022-08-25 18:10:57 +0200

    Add logo - Add genre db - Edit style body

--- db.php
+++ db.php
@@ -4,71 +4,81 @@
           'title' => 'New Jersey',
           'author' => 'Bon Jovi',
           'year' => 1988,
-          'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+          'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+          'genre' => 'rock',
         ],
    
         [
           'title' => 'My Wife Has A New GF',
           'author' => 'NOFX',
           'year' => 2020,
-          'poster' => 'https://img.discogs.com/N3V_iWktFiMYHLEQ4xScIH_y8XA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14782487-1581511291-8492.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/N3V_iWktFiMYHLEQ4xScIH_y8XA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14782487-1581511291-8492.jpeg.jpg',
+          'genre' => 'rock',
         ],
    
         [
           'title' => 'Texas Sun',
           'author' => 'Khruangbin & Leon Bridges',
           'year' => 2020,
-          'poster' => 'https://img.discogs.com/Cp6ErvJoQaqDw5u53DqjMuE3lag=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14753413-1581584971-6022.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/Cp6ErvJoQaqDw5u53DqjMuE3lag=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14753413-1581584971-6022.jpeg.jpg',
+          'genre' => 'soul',
         ],
    
         [
           'title' => 'Hotspot',
           'author' => 'Pet Shop Boys',
           'year' => 2020,
-          'poster' => 'https://img.discogs.com/Q5AAey3roJ3JzHVqCIopYkaFJaY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14692292-1579841765-8334.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/Q5AAey3roJ3JzHVqCIopYkaFJaY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14692292-1579841765-8334.jpeg.jpg',
+          'genre' => 'elettropop',
         ],
    
         [
           'title' => 'Aretha Now',
           'author' => 'Aretha Franklin',
           'year' => 2020,
-          'poster' => 'https://img.discogs.com/W0SXjFvriRx84xyUeDbZwXAxqCg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14777443-1582168309-4361.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/W0SXjFvriRx84xyUeDbZwXAxqCg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14777443-1582168309-4361.jpeg.jpg',
+          'genre' => 'soul',
         ],
    
         [
           'title' => 'The Slow Rush',
           'author' => 'Tame Impala',
           'year' => 2020,
-          'poster' => 'https://img.discogs.com/ZUFR2NfnxIgUWYVSCj9DInMRl_Y=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14776380-1582384831-5812.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/ZUFR2NfnxIgUWYVSCj9DInMRl_Y=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14776380-1582384831-5812.jpeg.jpg',
+          'genre' => 'soft rock',
         ],
    
         [
           'title' => 'Blink-182',
           'author' => 'Blink-182',
           'year' => 2003,
-          'poster' => 'https://img.discogs.com/u5YBOmoYwFGQDa364-CEanVmZB4=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-371737-1500902599-9137.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/u5YBOmoYwFGQDa364-CEanVmZB4=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-371737-1500902599-9137.jpeg.jpg',
+          'genre' => 'pop punk',
         ],
    
         [
           'title' => '20',
           'author' => 'Capo Plaza',
           'year' => 2018,
-          'poster' => 'https://img.discogs.com/sbBvRoyozL6u8cbYl0hZH_9aYLo=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12051947-1527336330-3250.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/sbBvRoyozL6u8cbYl0hZH_9aYLo=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-12051947-1527336330-3250.jpeg.jpg',
+          'genre' => 'hip-hop',
         ],
    
         [
           'title' => 'Blur',
           'author' => 'Blur',
           'year' => 1997,
-          'poster' => 'https://img.discogs.com/nqHS20jIEdon20NHXAqNhlB9wXU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1289256-1293912980.jpeg.jpg'
+          'poster' => 'https://img.discogs.com/nqHS20jIEdon20NHXAqNhlB9wXU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1289256-1293912980.jpeg.jpg',
+          'genre' => 'indie',
         ],
    
         [
           'title' => 'Che Vita Meravigliosa',
           'author' => 'Diodato',
           'year' => 2020,
-          'poster' => 'https://img.discogs.com/ZUzkvOFjvso5tujiG8QsBhD0tec=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14791169-1581676510-7022.jpeg.jpg'
-        ]
+          'poster' => 'https://img.discogs.com/ZUzkvOFjvso5tujiG8QsBhD0tec=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14791169-1581676510-7022.jpeg.jpg',
+          'genre' => 'pop',
+          ]
      ];
 
      //var_dump($discsList);
--- version-html/main.js
+++ version-html/main.js
@@ -9,7 +9,7 @@ const app = new Vue({
     methods: {
         // Attraverso la chiamata axios recupero la lista dei dischi dal controller del db
         getDiscs() {
-           axios.get('http://localhost/php-ajax-dischi/calldb.php')
+           axios.get('http://localhost/php-ajax-dischi/calldb.php')  // Percorso relativo alla chiamata: '../calldb.php'
            .then(result => {
               this.discs = result.data;
               console.log(this.discs);
