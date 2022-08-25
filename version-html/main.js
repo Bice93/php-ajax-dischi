4946bb9892582eb10de92cc91b2a16145570de12
Bice93 <bianca.cerulo@hotmail.it>
2022-08-25 16:19:11 +0200

    Call axios

--- version-html/main.js
+++ version-html/main.js
@@ -5,4 +5,20 @@ const app = new Vue({
  
       ],
     },
+
+    methods: {
+        // Attraverso la chiamata axios recupero la lista dei dischi dal controller del db
+        getDiscs() {
+           axios.get('http://localhost/php-ajax-dischi/calldb.php')
+           .then(result => {
+              this.discs = result.data;
+              console.log(this.discs);
+           });
+        },
+     },
+ 
+    created: function() {
+        this.getDiscs();
+    },
+ 
   })
